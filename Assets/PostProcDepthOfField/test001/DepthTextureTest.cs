aa86b326cd99de3913bf3514825d4a48c2a981bf
wilker <364105996@qq.com>
2019-02-03 16:00:53 +0800

    a

---
+++ Assets/PostProcDepthOfField/test001/DepthTextureTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 参考文章: https://blog.csdn.net/puppet_master/article/details/52819874
+
+[ExecuteInEditMode]
+public class DepthTextureTest : PostEffectBase
+{
+    void OnEnable()
+    {
+		Debug.Log("--- OnEnable");
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().depthTextureMode &= ~DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_Material)
+        {
+            Graphics.Blit(source, destination, _Material);
+        }
+    }
+}
