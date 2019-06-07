37310dd3d4d2148cb769a221978b1ff747afe1ea
wilker <364105996@qq.com>
2019-06-07 17:07:24 +0800

    a

--- Assets/PostProcBlurMotion/Translating.cs
+++ Assets/PostProcBlurMotion/Translating.cs
@@ -1,30 +1,33 @@
-using UnityEngine;  
-using System.Collections;  
-  
-public class Translating : MonoBehaviour {  
-  
-    public float speed = 10.0f;  
-    public Vector3 startPoint = Vector3.zero;  
-    public Vector3 endPoint = Vector3.zero;  
-    public Vector3 lookAt = Vector3.zero;  
-    public bool pingpong = true;  
-  
-    private Vector3 curEndPoint = Vector3.zero;  
-  
-    // Use this for initialization  
-    void Start () {  
-        transform.position = startPoint;  
-        curEndPoint = endPoint;  
-    }  
-      
-    // Update is called once per frame  
-    void Update () {  
-        transform.position = Vector3.Slerp(transform.position, curEndPoint, Time.deltaTime * speed);  
-        transform.LookAt(lookAt);  
-        if (pingpong) {  
-            if (Vector3.Distance(transform.position, curEndPoint) < 0.001f) {  
-                curEndPoint = Vector3.Distance(curEndPoint, endPoint) < Vector3.Distance(curEndPoint, startPoint) ? startPoint : endPoint;  
-            }  
-        }  
-    }  
-}  
+using System.Collections;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Translating : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public bool pingpong = true;
+    public Transform startTF;
+    public Transform endTf;
+
+    private Vector3 curEndPoint = Vector3.zero;
+
+    private Vector3 startPoint = Vector3.zero;
+    private Vector3 endPoint = Vector3.zero;
+
+    void Start() {
+        startPoint = startTF.position;
+        endPoint = endTf.position;
+
+        transform.position = startPoint;
+        curEndPoint = endPoint;
+    }
+
+    void Update() {
+        transform.position = Vector3.Slerp(transform.position, curEndPoint, Time.deltaTime * speed);
+        if (pingpong) {
+            if (Vector3.Distance(transform.position, curEndPoint) < 0.001f) {
+                curEndPoint = Vector3.Distance(curEndPoint, endPoint) < Vector3.Distance(curEndPoint, startPoint) ? startPoint : endPoint;
+            }
+        }
+    }
+}
