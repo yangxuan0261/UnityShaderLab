51f27c4f18676a8d7162e8fb347b6beca6c13882
wilker <364105996@qq.com>
2019-06-19 23:44:31 +0800

    'Robot-yangx'

--- Assets/LodGroup/ShaderLod.cs
+++ Assets/LodGroup/ShaderLod.cs
@@ -11,15 +11,27 @@ public class ShaderLod : MonoBehaviour {
         mat = transform.GetComponent<Renderer>().sharedMaterial;
     }
 
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.A)) {
-            mat.shader.maximumLOD = 500;
             Debug.LogFormat("--- maximumLOD = 500");
+            mat.shader.maximumLOD = 500;
         }
 
         if (Input.GetKeyDown(KeyCode.B)) {
-            mat.shader.maximumLOD = 600;
             Debug.LogFormat("--- maximumLOD = 600");
+            mat.shader.maximumLOD = 600;
+        }
+
+        // globalMaximumLOD 貌似没什么乱用
+        if (Input.GetKeyDown(KeyCode.C)) {
+            Debug.LogFormat("--- globalMaximumLOD = 500");
+            Shader.globalMaximumLOD = 500;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            Debug.LogFormat("--- globalMaximumLOD = 600");
+            Shader.globalMaximumLOD = 600;
         }
     }
 }
