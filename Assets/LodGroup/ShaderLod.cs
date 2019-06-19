0a98f7e70bfa9a6d497a60f8e51ccee91ef8c195
wilker <364105996@qq.com>
2019-06-19 23:23:35 +0800

    a

---
+++ Assets/LodGroup/ShaderLod.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderLod : MonoBehaviour {
+
+    Material mat;
+
+    void Start() {
+        // Shader.globalMaximumLOD = 600;
+        mat = transform.GetComponent<Renderer>().sharedMaterial;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            mat.shader.maximumLOD = 500;
+            Debug.LogFormat("--- maximumLOD = 500");
+        }
+
+        if (Input.GetKeyDown(KeyCode.B)) {
+            mat.shader.maximumLOD = 600;
+            Debug.LogFormat("--- maximumLOD = 600");
+        }
+    }
+}
---
+++ Assets/LodGroup/ShaderLod.shader
@@ -0,0 +1,19 @@
+﻿Shader "test/testShaderLod" {
+    Properties {
+        _MainTex ("Base (RGB)", 2D) = "white" {} 
+    }
+
+    SubShader 
+    {       
+        Tags { "RenderType"="Opaque" "IgnoreProjector"="True"}
+        LOD 600
+        UsePass "test/PassNameBase/PassNameBase02" // green
+    } 
+
+    SubShader 
+    {       
+        Tags { "RenderType"="Opaque" "IgnoreProjector"="True"}
+        LOD 500
+        UsePass "test/PassNameBase/PassNameBase01" // red
+    } 
+}
